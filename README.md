5ca4920fc4093b3d00054637ce91e8122322383d
Theo Daly <theo.daly@outlook.com>
2018-10-04 18:16:03 -0500

    all files

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # TriviaGame
+
+## What is this repo project?
+
+* This repo is a fun game called Trivia!
+* The player will have to guess correct to trivia questions in order to win the game.
+
+## How does it work?
+
+* Create a trivia form with multiple choice or true/false options.
+
+* The player will have a limited amount of time to finish the quiz. 
+
+  * The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+* Don't let the player pick more than one answer per question.
+
+* Don't forget to include a countdown timer.
+
+## Who will use this repo?
+
+* This is for coursework utilizing Javascript and jQuery, but the game is open to anyone who wants to explore the code or play for fun.
+
+## What is the goal of the project?
+
+* create a Trivia game using JavaScript for the logic and jQuery to manipulate HTML.
---
+++ assets/javascript/app.js
@@ -0,0 +1,84 @@
+//start quiz needs to make body box visible
+//answer you click needs to compare to correct answer
+//if right, add to correct answers
+//add a one minute timer that tallies results at 0
+//on click event to tally results for submit answers
+$(document).ready(function () {
+
+    alert('Welcome to DINO TRIVIA! Choose the correct answers for the best score.');
+
+});
+
+//set variables
+var correct = 0;
+var incorrect = 0;
+
+//if the right answer selected, adjust tally
+
+
+if (document.getElementById('right1').checked) {
+    correct++;
+}
+else {
+    incorrect++;
+    console.log();
+};
+if (document.getElementById('right2').checked) {
+    correct++;
+}
+else {
+    incorrect++;
+    console.log();
+};
+if (document.getElementById('right3').checked) {
+    correct++;
+}
+else {
+    incorrect++;
+    console.log();
+};
+if (document.getElementById('right4').checked) {
+    correct++;
+}
+else {
+    incorrect++;
+    console.log();
+};
+if (document.getElementById('right5').checked) {
+    correct++;
+}
+else {
+    incorrect++;
+    console.log();
+};
+
+//timer
+
+$("<div id='timer'>")
+
+var number = 120;
+var intervalId;
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+    number--;
+
+    $("#timer").html("<h2>" + number + "</h2>");
+
+    if (number === 0) {
+
+        stop();
+
+        alert("Time Up! Check your score");
+    }
+}
+
+function stop() {
+
+    clearInterval(intervalId);
+}
+
+run();
